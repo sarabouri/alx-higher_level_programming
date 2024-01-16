af061021657309aea04aa1705f4758690316ac62
sarabouri <sarabouri18@gmail.com>
2024-01-16 19:43:20 +0000

    m

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 i = 97
 for i in range(97, 123):
-    if i == 101 or i == 113:
+    if chr(i) == 101 and chr(i) == 113:
         continue
     else:
         print(f"{chr(i)}", end="")
