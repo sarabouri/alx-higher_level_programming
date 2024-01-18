c5a1c1cf87fc4e0d7c63a3d1600ae5cd946b937e
sarabouri <sarabouri18@gmail.com>
2024-01-18 20:36:51 +0000

    m

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     result = 0
 
     for i in range(len(sys.argv) - 1):
-        result += int(sys.argv[i + 1]):
+        result += int(sys.argv[i + 1])
     print("{}".format(result))
