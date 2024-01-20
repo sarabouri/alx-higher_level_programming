b48c878220dbfc851f76ce1da794ccc89a26b94c
sarabouri <sarabouri18@gmail.com>
2024-01-20 22:06:50 +0000

    m

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence[0] == None:
-        return None
+        return 0, None
     else:
         length = len(sentence)
         c = sentence[0]
