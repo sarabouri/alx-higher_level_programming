5a1ad266ab5889ec03da7674b30157c003e39868
sarabouri <sarabouri18@gmail.com>
2024-01-22 22:27:47 +0000

    m

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,14 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
-
+/**
+  * print_python_list_info - Print information about a Python list
+  * @p: Pointer to a Python object of type PyObject (assumed to be a list)
+  *
+  * This function prints information about a Python list, including its size,
+  * allocated memory, and the types of elements it contains. It is designed for
+  * debugging and inspecting Python lists from C code.
+ */
 void print_python_list_info(PyObject *p)
 {
 	long int size = PyList_Size(p);
