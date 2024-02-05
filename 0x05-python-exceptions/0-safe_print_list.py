4371de359366f1f2f7c97eff4342ca28ce6120fa
sarabouri <sarabouri18@gmail.com>
2024-02-05 23:04:45 +0000

    mandatiry tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    set = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i], end="")
-            set += 1
+            count += 1
         except IndexError:
             break
     print("")
-    return (set)
+    return (count)
