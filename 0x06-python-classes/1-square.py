c0f522ded612250a8494a625c690310eb2889035
sarabouri <sarabouri18@gmail.com>
2024-02-06 19:05:04 +0000

    m

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a Class Square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialization of the square size """
+    self.__size = size
