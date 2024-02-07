01212786d4122c0376d86002f99d0893355ce951
sarabouri <sarabouri18@gmail.com>
2024-02-07 23:03:08 +0000

    m

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -53,11 +53,11 @@ void print_python_bytes(PyObject *p)
  *  print_python_list - gives data of the PyListObject
  *  @p: the PyObject
  */
-void print_python_llist(PyObject *p)
+void print_python_list(PyObject *p)
 {
 	Py_ssize_t size = 0;
 	PyObject *item;
-	int i + 0;
+	int i = 0;
 
 	fflush(stdout);
 	printf("[*] Python list info\n");
