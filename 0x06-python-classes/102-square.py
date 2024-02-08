3cc1273fad918d7e62b1c92be6188d31cdb6e2b7
sarabouri <sarabouri18@gmail.com>
2024-02-08 00:54:53 +0000

    m

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new square
-        
+
         Args:
             size (int): The size of the new square.
         """
@@ -18,7 +18,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,7 +26,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
@@ -42,7 +42,7 @@ class Square:
     def __lt__(self, other):
         """Define the < comparison to a Square."""
         return self.area() < other.area()
-    
+
     def __le__(self, other):
         """Define the <= comparison to a Square."""
         return self.area() <= other.area()
