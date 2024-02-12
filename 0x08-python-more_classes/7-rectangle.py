448257a5e973ce0205aba3c50ef1fb11ed3406bd
sarabouri <sarabouri18@gmail.com>
2024-02-12 23:33:20 +0000

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     @property
     def width(self):
         """Get/set the width of the Rectangle."""
-    return self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
