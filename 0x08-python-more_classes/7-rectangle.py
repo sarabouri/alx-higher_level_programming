6afbb8552b19c317404c10a597fa5d7cbb3f502f
sarabouri <sarabouri18@gmail.com>
2024-02-12 23:34:32 +0000

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).number_of_instances +=  1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
