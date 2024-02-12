98d53f8b23c26f4534754f14bdeaf4ee7c7b1f47
sarabouri <sarabouri18@gmail.com>
2024-02-12 23:44:32 +0000

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,12 +14,12 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-    """Initialize a new Rectangle.
+        """Initialize a new Rectangle.
 
-    Args:
-        width (int): The width of the new rectangle.
-        height (int): The height of the new rectangle.
-    """
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         type(self).number_of_instances += 1
         self.width = width
         self.height = height
