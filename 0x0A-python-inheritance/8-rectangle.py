304c1108511f86245ccc62973ae38d474f4e95c0
sarabouri <sarabouri18@gmail.com>
2024-03-25 09:11:04 +0100

    solving 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Rectangel Class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Description for Rectangle class """
     def __init__(self, width, height):
